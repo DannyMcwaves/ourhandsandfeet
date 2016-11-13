2bd4b9aa90d765795cc58ef13b4e5f2cec682112
Josh Sherman <joshua.v.sherman@gmail.com>
2016-11-13 15:46:55 -0500

    login logout and dashboard

---
+++ src/dashboard.js
@@ -0,0 +1,12 @@
+// import 'bootstrap';
+// import {inject} from 'aurelia-framework';
+// import {Router} from 'aurelia-router';
+// import AppRouterConfig from "./app.router.config";
+// import {FetchConfig} from 'aurelia-auth';
+// import {AuthService} from "aurelia-auth";
+// import {AuthorizeStep} from 'aurelia-router';
+// @inject(Router,FetchConfig, AuthService, AppRouterConfig)
+
+export class Dashboard {
+
+}
