7d26108f7ee0f40abea94baa60684a00a0c7fcd6
Josh Sherman <joshua.v.sherman@gmail.com>
2017-02-05 07:20:29 -0500

    trying to get aurelia-auth to accept env variables in produtions, minor style updates, and put back the bookshelf

--- src/bookshelf.js
+++ src/bookshelf.js
@@ -1,38 +1,29 @@
 import {inject} from 'aurelia-framework';
 import {HttpClient, json} from 'aurelia-fetch-client';
 
-//import { bindable } from 'aurelia-framework';
-
-const fetch = !self.fetch ? System.import('isomorphic-fetch') : Promise.resolve(self.fetch);
-const booksUrl = process.env.BackendUrl + '/book/getall';
-
 @inject(HttpClient)
-export class Bookshelf {
-  books = [];
 
-  constructor(getHttpClient){
-    this.getHttpClient = getHttpClient;
+export class Bookshelf {
+  constructor(httpClient){
+    this.httpClient = httpClient;
   }
 
-  async activate(){
-    await fetch;
-    const http = this.http = this.getHttpClient();
+  books=null;
+  getBooks(){
+    this.httpClient.fetch(process.env.BackendUrl + '/book/getall')
+    .then(response => response.json())
+    .then(data => {
 
-    http.configure(config => {
-      config
-        .useStandardConfiguration()
-        .withBaseUrl(booksUrl);
+      this.books=data;
     });
 
-    const respose = await http.fetch();
-    this.books = await response.json();
+  }
 
-    // this.httpClient.fetch()
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         //console.log("Books:");
-    //         console.log(data);
-    //         this.books = data;
-    //     });
+  // getBook(bookname){
+  //
+  // }
+
+  activate(){
+    this.getBooks();
   }
 }
