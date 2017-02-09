9516f5df8305eb644d366752cc93f78cbd2ff3b5
freeth <freethlua@gmail.com>
2017-02-09 15:25:17 +0530

    test: createBooksFromCSV()

--- test/unit/createBooks.spec.js
+++ test/unit/createBooks.spec.js
@@ -2,6 +2,7 @@ import {CreateBookDashboard} from '../../src/dashboard-routes/bookstore-routes/c
 import {HttpClient} from 'aurelia-fetch-client';
 import './setup';
 import {Router} from 'aurelia-router';
+import {csv as csvFixture} from './createBooks.spec.fixtures';
 
 class HttpStub extends HttpClient {
   status = 500;
@@ -10,19 +11,19 @@ class HttpStub extends HttpClient {
   returnKey;
   returnValue;
   responseHeaders = [];
-  
+
   fetch(input, init) {
     let request;
     //let response;
     let responseInit = {};
     responseInit.headers = new Headers();
-    
+
     for (let name in this.responseHeaders || {}) {
       responseInit.headers.set(name, this.responseHeaders[name]);
     }
-    
+
     responseInit.status = this.status || 200;
-    
+
     if (Request.prototype.isPrototypeOf(input)) {
       request = input;
     } else {
@@ -31,7 +32,7 @@ class HttpStub extends HttpClient {
     if (request.body && Blob.prototype.isPrototypeOf(request.body) && request.body.type) {
       request.headers.set('Content-Type', request.body.type);
     }
-    
+
     let promise = Promise.resolve();
     // .then( () => {
     //   if (request.headers.get('Content-Type') === 'application/json' && request.method !== 'GET') {
@@ -51,7 +52,7 @@ class HttpStub extends HttpClient {
 }
 
 class RouterStub extends Router {
-  
+
   navigate(destination) {
     return this.router.destination = destination;
     // return new Promise((resolve)=>{
@@ -61,35 +62,51 @@ class RouterStub extends Router {
 }
 
 describe('the createBook module', () => {
-  let sut = new CreateBookDashboard( new HttpStub(), new RouterStub() ); //We're using DI for our HttpClient
-  
-  beforeEach((done) => {
+  let sut, httpStub, routerStub, fileReaderStub;
+
+  beforeEach(() => {
+    httpStub = new HttpStub();
+    routerStub = new RouterStub();
+    fileReaderStub = {};
+    sut = new CreateBookDashboard( httpStub, routerStub, fileReaderStub ); //We're using DI for our HttpClient
     sut.httpClient.status = 200; //we'll check for errors later
     sut.httpClient.object = {id: '1', artist: 'Prince', record: 'Purple Rain'}; //this is what we expect to from the GET
     sut.httpClient.returnKey = 'date'; //key returned from PUT/CREATE/POST
     sut.httpClient.returnValue = '6/24/1984'; //value returned from PUT/CREATE/POST
     sut.httpClient.responseHeaders = {accept: 'json'};
-    done();
   });
-  
-  it('should post a new book from the form data', () => {
+
+  xit('should post a new book from the form data', () => {
     sut.newBook = {'title': 'testTitle', 'type': 'pdf'};
     sut.createBook();
     //expect(this.Res.status).toEqual(201);
     //done();
   });
-  
-  it('should post a new book from the form data as type equals book if a type is not defined', () => {
+
+  xit('should post a new book from the form data as type equals book if a type is not defined', () => {
     sut.newBook = {'title': 'howdy', 'type': 0};
     sut.createBook();
     //expect(record).toEqual({"title":"testTitle","id":"1","type":"pdf", date: '6/24/1984'})
     //done();
   });
-  
-  it('should convert from csv and then post that array of books', () => {
-    //sut.CSVFilePath =
-    //sut.createBooksFromCSV();
+
+  it('should convert from csv and then post that array of books', (done) => {
+    fileReaderStub.readAsText = () => {};
+    sut.CSVFilePath = { files: [csvFixture.string] };
+    sut.createBooksFromCSV();
+    sut.httpClient.fetch = (url, {body: blob}) => {
+      const reader = new FileReader();
+      reader.onload =  () => {
+        const data = new TextDecoder('utf8').decode(reader.result);
+        expect(JSON.parse(data)).toEqual(csvFixture.json);
+        done();
+      };
+      reader.readAsArrayBuffer(blob);
+      return new Promise(()=>{}); // never resolved
+    };
+    fileReaderStub.onload({ target: { result: csvFixture.string } });
   });
+
   // it('displays an modal to the user when a record cannot be saved', (done) => {
   //   sut.http.status = 400
   //   sut.http.statusText = "Record Not Found"
