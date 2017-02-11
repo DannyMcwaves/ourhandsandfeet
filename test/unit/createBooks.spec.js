f5923041da07060aebe4ced1fef6443d40feeff1
DannyMcwaves <dannymcwaves96@rocketmail.com>
2017-02-11 22:44:48 +0000

    added coverage for this.newBook.type

--- test/unit/createBooks.spec.js
+++ test/unit/createBooks.spec.js
@@ -76,6 +76,13 @@ describe('the createBook module', () => {
       done();
     });
 
+    it ("should log a new book type when book is undefined", done => {
+        bookdashboard.newBook.type = 0;
+        bookdashboard.createBook();
+        expect(http.status).toBe(200);
+        done();
+    })
+
     // trying another option for testing the createBooksFromCSV();
     it("should confirm a http status change", done => {
         window.CSVFilePath = {files: [new Blob([csvFixture.string])] };
@@ -85,6 +92,21 @@ describe('the createBook module', () => {
         bookdashboard.createBooksFromCSV();
         // if dashbook.createBooksFromCSV is called, it should called the makeLotaBooks that
         // places a http call and HttpMock will respond to it and also change the status.
+        setTimeout(function () {
+            expect(http.status).toBe(200);
+        }, 10);
+        done();
+    })
+
+    it("should confirm a http status change", done => {
+        window.CSVFilePath = {files: [new Blob()] };
+        let reader = new FileReader(),
+            http = new HttpMock();
+            bookdashboard = new CreateBookDashboard(http, router, reader);
+        bookdashboard.createBooksFromCSV();
+        // if dashbook.createBooksFromCSV is called, it should called the makeLotaBooks that
+        // places a http call and HttpMock will respond to it and also change the status.
+        reader.abort();
         setTimeout(function () {
             expect(http.status).toBe(200);
         }, 10);
