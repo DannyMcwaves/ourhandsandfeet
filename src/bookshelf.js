84aee5d3c68a7e3d43f909b6ac869c01614d4e94
Cole Inman <coletranemusic@gmail.com>
2017-02-18 12:14:18 -0500

    removed htt http client configs, full coverage now

--- src/bookshelf.js
+++ src/bookshelf.js
@@ -8,20 +8,14 @@ const fetch = !self.fetch ? System.import('isomorphic-fetch') : Promise.resolve(
 
 @inject(HttpClient)
 export class Bookshelf {
-  constructor(httpClient){
-    this.httpClient = httpClient;
-  }
 
-  async activate(){
-    await fetch;
+    constructor(HttpClient) {
+        this.http = HttpClient;
+    }
 
-    this.httpClient.configure(config => {
-      config
-      .useStandardConfiguration()
-      .withBaseUrl(process.env.BackendUrl);
-    });
-
-    const res = await this.httpClient.fetch('/book/getall');
-    this.books =  await res.json();
-  }
+    async activate() {
+        await fetch;
+        const res = await this.http.fetch('/book/getall');
+        this.books = await res.json();
+    }
 }
