eafe3ea68920d301ae7d8fd3627a9eb5d1ff2df4
Josh Sherman <joshua.v.sherman@gmail.com>
2017-02-22 13:00:13 -0500

    code cleanup

--- test/unit/dashboard-router.spec.js
+++ test/unit/dashboard-router.spec.js
@@ -1,9 +1,9 @@
 import {DashboardRouter} from '../../src/dashboard-router';
 import {Router} from 'aurelia-router';
 
-class DashboardRouterStub extends DashboardRouter{
-  config={};
-}
+// class DashboardRouterStub extends DashboardRouter {
+//   config={};
+// }
 
 class ConfigStub {
   map(array1) {
@@ -20,7 +20,7 @@ class RouterStub {
     this.routes = routes;
   }
 
-  addPipelineStep(param1, param2){
+  addPipelineStep(param1, param2) {
     //do nothing
   }
 }
@@ -33,42 +33,17 @@ describe('the dashboard-router module', () => {
   mockedRouter = new RouterStub();
   const config1 = new ConfigStub;
   config1.map([
-    { route: "", name: 'dashboard', moduleId: './dashboard-routes/dashboard', nav: false, title: 'Dashboard', auth:true},
-    { route: "volunteer", name: 'volunteer', moduleId: './dashboard-routes/volunteer-dashboard', nav: false, title: 'Volunteer', auth:true},
-    { route: "charity", name: 'charity', moduleId: './dashboard-routes/charity-dashboard', nav: false, title: 'Charity', auth:true},
-    { route: "library", name: 'library', moduleId: './dashboard-routes/store-manager-dashboard-router', nav: false, title: 'Store', auth:true}
-    //{ route: "info", name: 'info',       moduleId: './dashboard-routes/info',       nav: true, title: 'Information', auth:true }
+    { route: '', name: 'dashboard', moduleId: './dashboard-routes/dashboard', nav: false, title: 'Dashboard', auth: true},
+    { route: 'volunteer', name: 'volunteer', moduleId: './dashboard-routes/volunteer-dashboard', nav: false, title: 'Volunteer', auth: true},
+    { route: 'charity', name: 'charity', moduleId: './dashboard-routes/charity-dashboard', nav: false, title: 'Charity', auth: true},
+    { route: 'library', name: 'library', moduleId: './dashboard-routes/store-manager-dashboard-router', nav: false, title: 'Store', auth: true}
+  
   ]);
   sut = new DashboardRouter(mockedRouter);
-  //sut.router = Router;
-  //sut.appRouterConfig = AppRouterConfig;
-  //console.log(sut);
-  //sut.configure();
-
-
-  // it('contains a router property', () => {
-  //   expect(sut.router).toBeDefined();
-  // });
 
   it('configures the dashboard child routes', (done) => {
     sut.configureRouter(config1, mockedRouter);
     expect(sut.router).toBeDefined;
     done();
   });
-
-  // it('should have an volunteer route', () => {
-  //   expect(sut.router.routes).toContain({ route: "volunteer", name: 'volunteer', moduleId: './dashboard-routes/volunteer-dashboard', nav: false, title: 'Volunteer', auth:true });
-  // });
-  //
-  // it('should have a charity route', () => {
-  //   expect(sut.router.routes).toContain({ route: "charity", name: 'charity', moduleId: './dashboard-routes/charity-dashboard', nav: false, title: 'Charity', auth:true });
-  // });
-  //
-  // it('should have a library route', () => {
-  //   expect(sut.router.routes).toContain({ route: "library", name: 'library', moduleId: './dashboard-routes/store-manager-dashboard-router', nav: false, title: 'Store', auth:true });
-  // });
-
-  // it('should have a login route', () => {
-  //   expect(sut.router.routes).toContain({ route: 'dashboard', name: 'dashboard', moduleId: './dashboard', nav: false, title: 'Dashboard', auth:true });
-  // });
 });
