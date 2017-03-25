f2614e11cd403bf61abf06866ff4c047b053efc2
Cole Inman <coletranemusic@gmail.com>
2017-03-25 13:00:10 -0400

    cleaned up main.js, got components feature working in prod

--- src/main.js
+++ src/main.js
@@ -8,50 +8,19 @@ import config from '../authConfig';
 // comment out if you don't want a Promise polyfill (remove also from webpack.common.js)
 
 import * as Bluebird from 'bluebird';
-Bluebird.config({ warnings: false });
+Bluebird.config({warnings: false});
 
 export async function configure(aurelia) {
-  if (process.env.NODE_ENV !== 'production'){
-    aurelia.use
+  aurelia.use
     .standardConfiguration()
     .developmentLogging()
-    .feature('components');
-    aurelia.use.plugin('aurelia-polymer');
-    aurelia.use.plugin('aurelia-auth', (baseConfig)=>{
+    .feature('components')
+    .plugin('aurelia-polymer')
+    .plugin('aurelia-auth', (baseConfig) => {
       baseConfig.configure(config);
     });
-    aurelia.use.plugin('au-table');
-    //aurelia.use.plugin('aurelia-files/dist/amd');
-    // Uncomment the line below to enable animation.
-    // aurelia.use.plugin('aurelia-animator-css');
-    // if the css animator is enabled, add swap-order="after" to all router-view elements
-    // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-    // aurelia.use.plugin('aurelia-html-import-template-loader')
-    document.addEventListener('WebComponentsReady', function() {
-      aurelia.start().then(() => aurelia.setRoot('app'));
-    });
-    //await aurelia.start();
-    //aurelia.setRoot('app');
-  } else {
-    aurelia.use
-    .standardConfiguration()
-    .developmentLogging();
-    aurelia.use.plugin('aurelia-polymer');
-    //aurelia.use.plugin('aurelia-files/dist/amd');
-    aurelia.use.plugin('au-table');
-    aurelia.use.plugin('aurelia-auth', (baseConfig)=>{
-      baseConfig.configure(config);
-    });
-    document.addEventListener('WebComponentsReady', function() {
-      aurelia.start().then(() => aurelia.setRoot('app'));
-    });
-    // aurelia.start().then(() => aurelia.setRoot("app"));
-    // aurelia.setRoot('app');
-  }
-  // if you would like your website to work offline (Service Worker),
-  // install and enable the @easy-webpack/config-offline package in webpack.config.js and uncomment the following code:
-  /*
-  const offline = await System.import('offline-plugin/runtime');
-  offline.install();
-  */
+
+  document.addEventListener('WebComponentsReady', function () {
+    aurelia.start().then(() => aurelia.setRoot('app'));
+  });
 }
